fcaebb73048eb38ed80ef6641eea047427921f60
T-User <T-User@T-PC05>
2016-10-14 17:19:09 +0900

    10/14_17:15

---
+++ src/DB_Object_Factory.java
@@ -0,0 +1,7 @@
+
+
+public abstract class DB_Object_Factory{
+
+    public abstract DB_Object factoryMethod();
+
+}
---
+++ src/PlanePosition.java
@@ -0,0 +1,10 @@
+
+public class PlanePosition {
+	double Lon;
+	double Lat;
+	PlanePosition(double Lon, double Lat){
+		this.Lon = Lon;
+		this.Lat = Lat;
+	}
+
+}
