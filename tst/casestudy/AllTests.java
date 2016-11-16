a4aa68b32322ede75b4dd810e1893436d1792f83
I-User <I-User@I-PC02>
2016-11-16 13:07:54 +0900

    PlanePositionTest作成

--- tst/casestudy/AllTests.java
+++ tst/casestudy/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ EvenAndOddMatcherTest.class, HexToBinaryTest.class, TestDataReadTest.class, TypeCodeTest.class })
+@SuiteClasses({ EvenAndOddMatcherTest.class, HexToBinaryTest.class, TestDataReadTest.class, TypeCodeTest.class, PlanePositionFactoryTest.class })
 public class AllTests {
 
 }
