2cba4f8185fcf517490f6f5d486e762596501dd2
T-User <T-User@T-PC05>
2016-11-24 14:40:05 +0900

    EvenAndOddMatcher ソース内にコメント記述

--- src/casestudy/EvenAndOddMatcher.java
+++ src/casestudy/EvenAndOddMatcher.java
@@ -15,13 +15,18 @@ public class EvenAndOddMatcher {
 	static ArrayList<Data> evenDataList = new ArrayList<Data>();
 	static ArrayList<Data> oddDataList = new ArrayList<Data>();
 
+	//受信した受信した生データ（16進数表記かつスペースで区切られている）を解析し、各DB-Itemを作る
 	public static String analyzeData(String hexRawData){
 
 		if(hexRawDataCheck(hexRawData)){
 
 			String binaryRawData = HexToBinary.hexToBinary(hexRawData);
 			PlanePosition planePosition = null;
+
+			//受信データのパリティチェック
 			if(parityCheck(binaryRawData)){
+
+				//ADS-Bデータか判別
 				if(judgedADS_B_Data(binaryRawData)){
 
 					if(			createTypeCode(binaryRawData) == CALL_SIGN){
@@ -40,19 +45,22 @@ public class EvenAndOddMatcher {
 		return null;
 	}
 
-	private static boolean judgedADS_B_Data(String rawData) {
-		return createDownLinkFormatNo(rawData) == 17;
-	}
-
-	private static int createDownLinkFormatNo(String data) {
-		return Integer.parseInt(data.substring(56,56+5), 2);
-	}
-
+	//受信した生データ（16進数表記かつスペースで区切られている）がADS-Bデータのときtrue、それ以外はfalseを返す
 	private static boolean hexRawDataCheck(String hexRawData){
 		if(hexRawData.length() == 75){
 			return hexRawData.substring(0,0+8).equals("10 02 01") && hexRawData.substring(63,63+5).equals("10 03");
 		}
 		return  false;
+	}
+
+	//バイナリ形式の受信データのDF(ダウンリンクフォーマット)をint型で返す
+	private static int createDownLinkFormatNo(String data) {
+		return Integer.parseInt(data.substring(56,56+5), 2);
+	}
+
+	//DF(ダウンリンクフォーマット)が17のときtrue、それ以外はfalseを返す
+	private static boolean judgedADS_B_Data(String rawData) {
+		return createDownLinkFormatNo(rawData) == 17;
 	}
 
 }
